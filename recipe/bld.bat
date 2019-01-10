207d184d1e5b01c7a8c894c2391d1822c3084e72
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-09 23:50:59 -0700

    Add dummy.bat to populate for loops

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,12 +3,20 @@
 copy osgf_env.bat %LIBRARY_BIN%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+set "CONDA_ACTIVATE_D=%LIBRARY_PREFIX%\etc\conda\activate.d"
+if not exist "%CONDA_ACTIVATE_D%" mkdir "%CONDA_ACTIVATE_D%"
+copy dummy.bat "%CONDA_ACTIVATE_D%"
+
 
 :: OSGeo4W compatible envs
-:: Large apps, e.g. QGIS, launch scripts look for these
+:: Large apps, e.g. QGIS, have wrapper launch scripts that use these
 for %%g in (o4w_env.bat py3_env.bat qt5_env.bat) do (
   copy %%g %LIBRARY_BIN%
   if %errorlevel% neq 0 exit /b %errorlevel%
 )
+
+set "LIBRARY_INI=%LIBRARY_PREFIX%\etc\ini"
+if not exist "%LIBRARY_INI%" mkdir "%LIBRARY_INI%"
+copy dummy.bat "%LIBRARY_INI%"
 
 exit /b 0
---
+++ recipe/dummy.bat
@@ -0,0 +1,2 @@
+rem This is just a dummy to make sure for loops
+rem always have some files to process.
