1920715fe5912c2bf766f5f9b1a97054840fc2e2
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-23 12:46:25 -0700

    Remove migrate CONDA_PREFIX to CONDA_ROOT (conflicts during build)

--- recipe/envs/osgf_env.bat
+++ recipe/envs/osgf_env.bat
@@ -1,4 +1,4 @@
-REM NOTE: assumes this file is in CONDA_PREFIX\LIBRARY_PREFIX\bin
+REM NOTE: assumes this file is in CONDA_ROOT\LIBRARY_PREFIX\bin
 
 
 REM Some convenience vars
@@ -14,13 +14,16 @@ if "%LIBRARY_PREFIX%" == "" (
     for %%i in ("%CD%") do set LIBRARY_PREFIX=%%~fsi
   popd
 )
-if "%CONDA_PREFIX%" == "" (
+if "%CONDA_ROOT%" == "" (
   pushd %~dp0
     cd ..\..
-    REM Set CONDA_PREFIX to short path version
-    for %%i in ("%CD%") do set CONDA_PREFIX=%%~fsi
+    REM Set CONDA_ROOT to short path version
+    for %%i in ("%CD%") do set CONDA_ROOT=%%~fsi
   popd
 )
+
+REM Don't mess with anything else if called during 'conda build'
+if not "%CONDA_BUILD%" == "" goto :EOF
 
 cd %~dp0
 
@@ -28,32 +31,29 @@ REM Standard Win Library locations
 set "LIBRARY_BIN=%LIBRARY_PREFIX%\bin"
 set "LIBRARY_INC=%LIBRARY_PREFIX%\include"
 set "LIBRARY_LIB=%LIBRARY_PREFIX%\lib"
-set "SCRIPTS=%CONDA_PREFIX%\Scripts"
-
-REM Unix style path separators, for CMake, etc.
-set CONDA_ROOT=%CONDA_PREFIX:\=/%
+set "SCRIPTS=%CONDA_ROOT%\Scripts"
 
 REM Load the prefix's environment
-if exist "%CONDA_PREFIX%\Scripts\activate.bat" (
+if exist "%CONDA_ROOT%\Scripts\activate.bat" (
 
   REM Load conda env via prefix
-  "%CONDA_PREFIX%\Scripts\activate.bat" "%CONDA_PREFIX%"
+  "%CONDA_ROOT%\Scripts\activate.bat" "%CONDA_ROOT%"
 
 ) else (
 
   REM conda not available (e.g. bare pkgs installed from constructor)
   REM Set up fake conda env...
 
-  set "CONDA_PYTHON_EXE=%CONDA_PREFIX%\python.exe"
+  set "CONDA_PYTHON_EXE=%CONDA_ROOT%\python.exe"
   set "PYTHONNOUSERSITE=1"
   set "PYTHONIOENCODING=1252"
-  set "SYS_PREFIX=%CONDA_PREFIX%"
-  set "SYS_PYTHON=%CONDA_PREFIX%\python.exe"
+  set "SYS_PREFIX=%CONDA_ROOT%"
+  set "SYS_PYTHON=%CONDA_ROOT%\python.exe"
 
-  set "STDLIB_DIR=%CONDA_PREFIX%\Lib"
-  set "SP_DIR=%CONDA_PREFIX%\Lib\site-packages"
+  set "STDLIB_DIR=%CONDA_ROOT%\Lib"
+  set "SP_DIR=%CONDA_ROOT%\Lib\site-packages"
 
-  path "%CONDA_PREFIX%;%LIBRARY_PREFIX%\mingw-w64\bin;%LIBRARY_PREFIX%\usr\bin;%LIBRARY_BIN%;%SCRIPTS%;%PATH%"
+  path "%CONDA_ROOT%;%LIBRARY_PREFIX%\mingw-w64\bin;%LIBRARY_PREFIX%\usr\bin;%LIBRARY_BIN%;%SCRIPTS%;%PATH%"
 
   REM Load activate scripts
   if exist "%LIBRARY_PREFIX%\etc\conda\activate.d" (
--- recipe/envs/py3_env.bat
+++ recipe/envs/py3_env.bat
@@ -1,3 +1,11 @@
+if "%CONDA_ROOT%" == "" (
+  pushd %~dp0
+    cd ..\..
+    REM Set CONDA_ROOT to short path version
+    for %%i in ("%CD%") do set CONDA_ROOT=%%~fsi
+  popd
+)
+
 set PYTHONPATH=
-set PYTHONHOME=%CONDA_PREFIX%
-path %CONDA_PREFIX%;%CONDA_PREFIX%\Scripts;%PATH%
+set PYTHONHOME=%CONDA_ROOT%
+path %CONDA_ROOT%;%CONDA_ROOT%\Scripts;%PATH%
